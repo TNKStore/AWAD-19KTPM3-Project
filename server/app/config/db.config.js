dad08c5ae9b6902cf0c9091cd307be7575cfb1d8
thienxaso1st <tdht27092001@gmail.com>
2022-12-19 22:54:11 +0700

    Docker compose  update

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -3,6 +3,7 @@ module.exports = {
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
+  port: process.env.DB_DOCKER_PORT,
   dialect: "mysql",
   pool: {
     max: 5,
