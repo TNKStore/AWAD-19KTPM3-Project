55982cc7478306608c1e5014def786dcce8893c0
Trung Trần <tdht27092001@gmail.com>
2022-12-22 17:26:18 +0700

    Update flow socket

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -37,4 +37,9 @@ const User = sequelize.define("user", {
     freezeTableName: true
 });
 
+User.hasMany(Presentation, {
+    onDelete: "CASCADE"
+});
+Presentation.belongsTo(User);
+
 module.exports = User
